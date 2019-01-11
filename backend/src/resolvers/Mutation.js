b26bc0de5dee3fe9f8115cce82a8b43471f5466c
brahmdev <pandey.dev25@gmail.com>
2019-01-11 13:51:20 +0100

    backend updated with graphql and prisma and minor change in frontend

---
+++ backend/src/resolvers/Mutation.js
@@ -0,0 +1,10 @@
+const mutations = {
+    createDog(parent, args, ctx, info) {
+        global.dogs = global.dogs || [];
+        const newDog = {name: args.name};
+        global.dogs.push(newDog);
+        return newDog;
+    }
+};
+
+module.exports = mutations;
